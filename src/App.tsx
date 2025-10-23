8914a1981901508c7b84c68b4c0d1a95a536281f
Justine Apdal <justineapdal.edu@gmail.com>
2025-10-23 23:27:27 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import logo from './assets/icons/Logo-WHite.png'
+
+function App() {
+  return (
+    <div className="coming-soon-container">
+      <div className="content">
+        <img src={logo} alt="JA Logo" className="logo" />
+        <h1 className="main-title">COMING SOON</h1>
+        <p className="subtitle">Get ready! greatness takes time, but it's almost here.</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
